e6d20086cc89753442c1b58c29d6208f09e74d2f
Ayushdeval821 <devalayush25@gmail.com>
2026-04-13 12:05:40 +0530

    update

--- models/text_to_speech.py
+++ models/text_to_speech.py
@@ -1,12 +1,12 @@
-# from gtts import gTTS
-# import os
-# import time
+from gtts import gTTS
+import os
+import time
 
-# def generate_audio(text, lang='hi'):
-#     filename = f"translated_{int(time.time())}.mp3"
-#     output_path = os.path.join("static", "outputs", "speech", filename)
+def generate_audio(text, lang='hi'):
+    filename = f"translated_{int(time.time())}.mp3"
+    output_path = os.path.join("static", "outputs", "speech", filename)
 
-#     tts = gTTS(text=text, lang=lang)
-#     tts.save(output_path)
+    tts = gTTS(text=text, lang=lang)
+    tts.save(output_path)
 
-#     return filename
+    return filename
